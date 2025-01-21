30b02fce11d1732665c7a6833c7f47f53e0a0b97
Daniel <daniel.ruiz@qaracter.com>
2025-01-21 10:52:08 +0000

    Initial info Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+import java.lang.InterruptedException;
 
+
+public class Menu {
+    private Maquina maquina;
+    private Scanner scanner;
+
+    // Constructor
+    public Menu() {
+        this.maquina = new Maquina();
+        this.scanner = new Scanner(System.in);
+    }
